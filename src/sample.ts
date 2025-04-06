18569951dfc9c15fef46278047237e905413f88f
Anthony Charles <anthonylannn@gmail.com>
2025-04-06 16:49:13 +0200

    genesis

---
+++ src/sample.ts
@@ -0,0 +1,13 @@
+const author = {
+    name: `Angela`,
+    nickname: `angie`
+}
+
+
+const buy = {
+    title: `Purchase—$39.99`,
+    link: `https://buy.stripe.com/bIY5klbiBgZx2bKcMS`
+}
+
+
+export { author, buy }
