ccb8862bcfb35fb5ca3c6ad3270210bb751c41b1
Your Name <you@example.com>
2025-10-20 14:54:06 +0530

    initial commit

--- src/sample.ts
+++ src/sample.ts
@@ -1,10 +1,8 @@
 const author = {
-    name: `Angela`,
-    nickname: `angie`,
-    tiktok: `https://www.tiktok.com/@codetonero`,
-    ig: `https://www.instagram.com/codetonero/`,
-    x: `https://x.com/codetonero`,
-    email: 'mailto:anthonylannn@gmail.com'
+    name: `Prit`,
+    nickname: `prit`,
+    ig: `https://www.instagram.com/pritkr20/`,
+    email: 'mailto:pritkumarrrr@gmail.com'
 
 }
 
